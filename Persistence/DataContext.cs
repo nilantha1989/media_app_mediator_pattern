e4c3a9ec7bbe5a6480575e5498abbc726f17f7ad
nilantha1989 <nilantha.sac@gmail.com>
2024-10-28 14:58:15 -0600

    Inirial commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions options):base(options) 
+        {
+            
+        }
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+                .HasData(
+                    new Value { Id = 1, Name = "Val 1" },
+                    new Value { Id = 2, Name = "Val 2" },
+                    new Value { Id = 3, Name = "Val 3" }
+                );
+        }
+    }
+}
